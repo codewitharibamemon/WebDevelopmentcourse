59410c18cc41d4e0e8fa03f445fe3668b745aec2
codewitharibamemon <codewitharibamemon@gmail.com>
2024-12-22 23:50:17 +0500

    classcode

---
+++ class12/script.js
@@ -0,0 +1,211 @@
+// // // // alert("Hello world");
+
+// // // //ES5 
+
+// // // // variables
+
+// // // // var shoerack = "redshoe"; //string 
+
+
+
+
+// // // // var myname = "Ariba";//string
+// // // // console.log(myname);
+
+
+// // // // var mystudent = 'sameer';//string 
+// // // // console.log(mystudent)
+
+
+// // // // var numbers = 3; //number
+// // // // console.log(numbers);
+
+
+
+// // // // var number1 = 2;
+// // // // var number2 = 4;
+// // // // console.log(number2 - number1);
+
+
+
+// // // // var my_variable = "Ariba" //legal
+
+// // // // // var 1my_example illegal
+
+
+// // // // var myName  = "Aribamemon"; 
+
+
+// // // // addition 
+// // // var num1 = 2; // datatype number
+// // // var num2 = 3; 
+// // // console.log(num1 + num2) 
+
+// // // //
+// // // var num1 = 3; // datatype number
+// // // var num2 = 2; 
+// // // console.log(num1 - num2) 
+
+// // // //multiply
+// // // var num1 = 3;
+// // // var num2 = 4;
+// // // console.log(num1 * num2);
+
+
+// // // //remindar //modulus
+
+// // // var num1 = 10;
+// // // var num2 = 3;
+// // // console.log(num1 % num2);
+
+// // // //divide
+// // // var x = 4;
+// // // var y = 2;
+// // // console.log(x / y);
+
+
+// // // var x = 5;
+// // // x++; // 6
+// // // // x + 1 = 6
+// // // console.log(x)
+
+
+
+
+// // // var y = 2;
+// // // y--; // 
+// // // console.log(y)
+
+
+
+// // // var firstName = "Ariba";
+// // // var lastName = "Memon";
+// // // console.log( firstName + lastName )
+
+
+// // // var myName = " Ariba";
+// // // var lastName = " Memon";
+// // // console.log("My Name is" + myName) // concatenation
+// // // console.log(myName + lastName) //concatenation
+
+
+
+
+// // //Post increment
+// // // var y = 4;
+// // // y++;
+// // // console.log(y);
+
+// // //post decrement 
+// // // var y = 4;
+// // // y--;
+// // // console.log(y);
+
+
+
+
+
+
+// // // var x = 2;
+// // // var z = x++ + --x ;
+// // //     //  2 + 2 = 4
+// // //     console.log(z);
+    
+
+
+
+
+
+// // // var x = 3;
+// // // var y = 2;
+// // // var w = x++ + --y + ++x + y++;
+// // //       //3   + 1   + 5  + 1 = 10 
+
+// // //       // 3 + 1   +4 + 2
+// // //       // 3+1+4+2
+// // //       // 3+1+5+1
+
+// // // console.log(w);
+
+
+
+
+
+
+
+// // //prompt user sy input leny main kam ata hai
+// // //function 
+// // // var myQuestion = prompt("What is your name");
+// // // console.log(myQuestion);
+
+
+
+
+
+// // // var num = 2 + (3 * 4);
+// // // console.log(num);
+// // var r = 43;
+// // var y = 65;
+// // var z = ++r - r++ - y-- + --r + ++r - ++y ;
+// // console.log(z)
+
+
+
+
+
+// // function 
+// //ek block of code hota hai jo specific task perform karta hai 
+
+// // function keyword , usky bad uska nam sameer,ariba,asim
+// //round bracket hai usmy ap jo bhi cheez pass krengy ap usko parameter bolengy
+// // function App(name){
+// //  console.log('My name is ' + name)
+// // }
+// // App("Ariba")  //invoke ya call kaha jata hai 
+// //argument dia hai 
+
+
+
+// // function myCar(mycarname = "Alto" ){
+// //    console.log(`my car name is ${mycarname}`)
+// // //    console.log(`my studentcar name is ${mystudentcar}`)
+// // }
+// // myCar("landcruiser"); //
+// // // myCar();
+
+
+
+// //arrow function es6
+// // => arrow funtion 
+// // var greet = (name) =>{
+// //  console.log('My name is' + name);
+// //  }
+// //  greet("Ariba")
+
+
+// // variable ,function ka name  parameter arrow function
+//     var myCar = (carname) => {
+//         console.log(`my car name is ${carname}`);
+//         }
+//         myCar("alto");
+
+
+
+
+
+function showmsg(){
+    var name = document.getElementById
+}
+
+
+
+
+
+
+
+
+
+
+// function displayMsg(){
+//     alert('Added shirt')
+// }
---
+++ class18/script.js
@@ -0,0 +1,381 @@
+// // // // console.log("Hello ariba")
+
+// // // //Datatypes in js 
+// // // //string 
+
+// // // var my_Name = "Ariba"; //string 
+// // // //double ya single string 
+// // // var my_Goodname = 'Ariba'; //string
+// // // // "", '' //string
+
+
+// // // //number 12 ,233 
+// // // var num = 123 ; //number
+// // // // console.log(num)
+
+// // // //boolean //true or false
+// // // // var my_Student = true;
+// // // // console.log(my_Student)
+
+// // // // var mystudent_Asma = false;
+// // // // console.log(mystudent_Asma)
+
+
+
+// // // //null 
+// // // //baad m jakar ap usko value dengy
+// // // var myShirt = null;
+// // // console.log(myShirt); // baaad m 1000 rupess lagadengy
+
+
+
+
+
+// // // var myNum = '13';
+
+// // // var myNum = BigInt(42301828282828);
+// // // console.log(myNum)
+
+
+// // // var my_Value = "sameer";
+// // // console.log(typeof(my_Value))
+
+
+// // // console.log( 10 + (4 * 3));//Bodmas 
+// // //           // 10 +   12 = 22
+
+
+
+
+
+// // // console.log(5 - (10 * 4))
+// // //           // 5 -  40 = -35
+           
+
+
+
+
+// // // var my_Name = "AribaMemon"; //
+// // // var last_Name = "Memon"
+// // // // console.log(my_Name.toUpperCase());
+// // // // console.log(my_Name.toLowerCase());
+// // // // console.log(my_Name.concat(" ",last_Name))
+// // // console.log(my_Name.length);// 
+
+
+
+
+
+
+
+// // // var myStudent1 = "Sameer";
+// // // var myStudent2 = "Asim";
+// // // var myStudent3 = "Abeera";
+// // // var myStudent4 = "Umema";
+// // // var myStudent5 = "Maryam";
+
+// // // //Array list banana // 0 
+// // // var Mystudents = ["Sameer", "Asim","Abeera","Umema","Sobia"];
+// // //                  // 0 index , 1 index ,2    ,3    , 4 
+
+// // //  console.log(Mystudents.push("Maryam"))
+ 
+// // //  console.log(Mystudents.pop())
+// // //  console.log(Mystudents);
+
+// // // console.log(Mystudents[0]);
+// // // console.log(Mystudents.length);
+// // // console.log(Mystudents.shift());
+
+// // //  console.log(Mystudents.unshift("Maryam"));
+
+
+
+// // // Objects //
+
+// // // key = value 
+
+// // // var my_Car ={
+// // //     name :"Alto",
+// // //     modal : "55A",
+// // //     weight : 550,
+// // // }
+// // // console.log(my_Car.name)
+
+// // // var mystudent_Abeera ={
+// // //     name : "Abeera",
+// // //     age: 16,
+// // //     id : 1234,
+// // //     adress: "lahore"
+
+// // // }
+// // // console.log(mystudent_Abeera.age)
+
+
+
+
+
+
+
+// // // > greater than
+// // // < less than
+
+
+// // //equal to
+// // // 5 == 8 ; false 
+// // // 5 == 5 ; true 
+
+
+// // // 5 > 8; //false 
+// // // 5 < 8;//true
+
+
+// // // var x = 5;
+// // // var b = "5";
+// // // var c = x === b;
+// // // console.log(c);
+
+// // //== equal to ky operator main datatype nh check hote hai 
+// // // === ismy data type bhi check hoge or yeh bhi check hoga ky barabar hai ya nh
+
+
+
+// // // var x = "5";//string
+// // // if(x ===5){
+// // //     console.log("Number is correct");
+// // // }
+// // // else{
+// // //     console.log("INcoreect")
+// // // }
+
+// // // var userName= prompt("your name");
+// // // var userAge= prompt("your age");
+// // // console.log(userName);
+// // // console.log(userAge);
+
+
+
+
+
+
+
+// // // var num = 5;
+// // // var num1 = 6;
+
+// // // if(num < num1){
+// // //     console.log('5 is greater than 6');
+// // //     //  block of code to be executed if the condition is true
+// // // }
+// // // else{
+// // //     console.log('6 is greater than 5');
+// // //     //  block of code to be executed if the condition is false
+// // // }
+
+
+// // // var cradle_kids = 19;
+// // // if (cradle_kids < 10) {
+// // //    console.log("beth jao");
+   
+// // //   } 
+// // //   else if (cradle_kids > 18){
+// // //   console.log('beth jao tum bhi')
+// // //   }
+  
+// // //   else {
+// // //     console.log( "nh betho");
+    
+// // //   }
+
+
+// // // var cradle_kids = 8;
+// // // var cradle_adult = 18;
+// // // if(cradle_kids > 18){
+// // // console.log('baith jao');
+
+// // // }
+// // // else if(cradle_adult > 18){
+// // //     console.log('baray beth jayein');
+    
+// // // }
+// // // else{
+// // //     console.log('nh baith sakty');
+    
+// // // }
+
+
+// // //functions
+
+// // // function my_Servant(){
+// // //     console.log('Chai banadega')
+// // // }
+
+// // // my_Servant()// call ya invoke
+
+
+
+
+
+// // // function ka keyword
+
+// // // function my_Students(){
+// // //     console.log('Sameer and asim present')
+// // //     // code to be executed
+// // // }
+// // // my_Students();
+
+
+
+
+
+
+// // // not equal !=
+// // //!==	not equal value or not equal type
+// // // var x = 5 
+// // // console.log(x !== '5'); // 
+
+
+
+
+// // //>=	greater than or equal to
+
+// // // var x = 5;
+// // // console.log(x >= 8);//false
+
+// // // //<=	less than or equal to
+// // // var x = 5;
+// // // console.log(x <= 8);  //true
+
+
+
+
+
+// // //Datatype undefined
+
+// // // var x; //value nh dein
+// // // console.log(x);
+
+
+// // //ES5 
+
+// // // var first_Name = 'Ariba';
+// // // var second_Name = 'Memon';
+// // // console.log(first_Name + second_Name);
+
+
+// // // //ES6 template literals
+// // // var first_Name = 'Ariba';
+// // // var second_Name = 'Memon';
+// // // console.log(`${first_Name} ${second_Name}`);
+
+// // //concatenation
+
+
+
+
+// // // var userName= prompt("your name");
+// // // document.write(userName)
+
+
+
+
+// // //event kyn aye thy js main 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // //for loop 
+// // // 1 to 5 counting ajaye 
+// // //var i = 1; initialization
+// // //condition
+// // for(var i = 1; i<=50;i++){
+// //  console.log(i);
+ 
+// // }
+
+
+// //ES5
+// //var 
+// //ES6 
+// // let ,const 
+
+// // var my_Name = "Ariba";
+// // var my_Name = "Memon";
+// // console.log(my_Name)
+ 
+// // let my_Name = "Ariba";
+// // let my_Name = "Memon";
+// // console.log(my_Name);
+
+
+// //const 
+// //redeclared
+// // const my_Name = "Ariba";
+// // const my_Name = "Memon";
+// // console.log(my_Name);
+
+
+
+// //reassigned
+// // var my_Student = "Sameer";
+// // my_Student = "Asim";
+// // console.log(my_Student); //allow
+
+// // let my_Student1 = "Sameer";
+// // my_Student1 = "Asim";
+// // console.log(my_Student1); //allow
+
+
+// // const my_Student2 = "Sameer";
+// // my_Student2 = "Asim";
+// // console.log(my_Student2); //not allowed
+
+
+
+
+// // console.log(x);// undefined
+
+// // var x = 10;
+// // console.log(x); //10
+
+
+// // console.log(a); //undefined
+
+// // var a = "Ariba";
+// // console.log(a); // Ariba
+
+
+// //Cannot access 'a' before initialization 
+// //Temporal dead zone (TDZ)
+// // console.log(a);
+
+// // let a = "Ariba";
+// // console.log(a); 
+
+// //Cannot access 'b' before initialization
+// //Temporal dead zone (TDZ)
+// console.log(b);
+
+// const b = "Ariba";
+// console.log(b); 
+
+//Math.random
+
+//Decimal number 0 to 1;
+// let number = Math.random();
+// console.log(number);
